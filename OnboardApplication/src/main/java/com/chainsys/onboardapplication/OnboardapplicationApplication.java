6dffa62f1243fc2cd33e8a742879aeac085dee22
Dharanidharan18 <dharni25092001@gmail.com>
2024-06-28 17:55:39 +0530

    Spring web project package created

---
+++ OnboardApplication/src/main/java/com/chainsys/onboardapplication/OnboardapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.chainsys.onboardapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnboardapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnboardapplicationApplication.class, args);
+	}
+
+}
---
+++ OnboardApplication/src/test/java/com/chainsys/onboardapplication/OnboardapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chainsys.onboardapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnboardapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
